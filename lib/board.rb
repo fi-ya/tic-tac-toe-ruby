2b2bbf1d4205c1329499f9241487e5dd5d2b2fda
fi-ya <safiaali@hotmail.com>
2022-02-22 12:46:34 +0000

    feat: add logic to display board skeleton

---
+++ lib/board.rb
@@ -0,0 +1,16 @@
+
+
+
+def display_board
+
+    puts row = ["   " "|" "   " "|" "   "]
+    puts separator = "-----------"
+    puts row
+    puts separator
+    puts row
+    
+end 
+
+
+
+display_board
