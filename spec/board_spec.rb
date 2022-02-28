f6aabd6e70e6787148ead8be31b0575d8327faba
fi-ya <safiaali@hotmail.com>
2022-02-28 12:21:27 +0000

    refactor: remove duplication of board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,50 +2,36 @@ require "board"
 
 describe Board do 
 
+    before :each do
+        @new_board = Board.new
+      end
+
     context "initialize" do
         it "method returns the string with correct number of index" do
-            new_board = Board.new
-            expect(new_board.board).to eq(["-","-","-","-","-","-","-","-","-"])
-            expect(new_board.board.length).to eq(9)
+            expect(@new_board.board).to eq(["-","-","-","-","-","-","-","-","-"])
+            expect(@new_board.board.length).to eq(9)
         end 
 
         it "should display the board" do
-            # Given - Arrange
-            new_board = Board.new
-            # When - Act
-            output = new_board.generate_board()
-
-            # Then - Assert
+            output = @new_board.generate_board()
             expect(output).to eq("   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n")
         end   
  
         it "should display an X in the top level corner" do
-            # Given - Arrange
-            new_board = Board.new
-            # When - Act
-            new_board.place_X(0)
-            # Then - Assert
-            output = new_board.generate_board()
+            @new_board.place_X(0)
+            output = @new_board.generate_board()
             expect(output).to eq(" X |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n")
         end
 
             it "should display an X in the top middle row" do
-            # Given - Arrange
-            new_board = Board.new
-            # When - Act
-            new_board.place_X(1)
-            # Then - Assert
-            output = new_board.generate_board()
+            @new_board.place_X(1)
+            output = @new_board.generate_board()
             expect(output).to eq("   | X |   \n-----------\n   |   |   \n-----------\n   |   |   \n")
         end
 
-        it "should display an X in the top middle row" do
-            # Given - Arrange
-            new_board = Board.new
-            # When - Act
-            new_board.place_X(2)
-            # Then - Assert
-            output = new_board.generate_board()
+        it "should display an X in the top right row" do
+            @new_board.place_X(2)
+            output = @new_board.generate_board()
             expect(output).to eq("   |   | X \n-----------\n   |   |   \n-----------\n   |   |   \n")
         end
 
