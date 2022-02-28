0a7069c6974323de84ceed423c1849e6672d2034
fi-ya <safiaali@hotmail.com>
2022-02-28 12:37:11 +0000

    test: display x on middle and bottom rows

--- lib/board.rb
+++ lib/board.rb
@@ -10,9 +10,9 @@ class Board
     def generate_board
         " #{@board[0]} | #{@board[1]} | #{@board[2]} \n" + 
         "-----------\n" +
-        " #{@board[3]} |   |   \n" +
+        " #{@board[3]} | #{@board[4]} | #{@board[5]} \n" +
         "-----------\n" +
-        "   |   |   \n"
+        " #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
     end
 
     def place_X(index)
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,7 +14,7 @@ describe Board do
             expect(output).to eq("   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n")
         end   
  
-        it "should display an X in the top level corner" do
+        it "should display an X in the top left corner" do
             @new_board.place_X(0)
             output = @new_board.generate_board()
             expect(output).to eq(" X |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n")
@@ -32,10 +32,40 @@ describe Board do
             expect(output).to eq("   |   | X \n-----------\n   |   |   \n-----------\n   |   |   \n")
         end
 
-        it "should display an X in the top right row" do
+        it "should display an X in the middle left row" do
             @new_board.place_X(3)
             output = @new_board.generate_board()
             expect(output).to eq("   |   |   \n-----------\n X |   |   \n-----------\n   |   |   \n")
+        end
+
+        it "should display an X in the middle middle row" do
+            @new_board.place_X(4)
+            output = @new_board.generate_board()
+            expect(output).to eq("   |   |   \n-----------\n   | X |   \n-----------\n   |   |   \n")
+        end
+
+        it "should display an X in the middle right row" do
+            @new_board.place_X(5)
+            output = @new_board.generate_board()
+            expect(output).to eq("   |   |   \n-----------\n   |   | X \n-----------\n   |   |   \n")
+        end
+
+        it "should display an X in the bottom left row" do
+            @new_board.place_X(6)
+            output = @new_board.generate_board()
+            expect(output).to eq("   |   |   \n-----------\n   |   |   \n-----------\n X |   |   \n")
+        end
+
+        it "should display an X in the bottom middle row" do
+            @new_board.place_X(7)
+            output = @new_board.generate_board()
+            expect(output).to eq("   |   |   \n-----------\n   |   |   \n-----------\n   | X |   \n")
+        end
+
+        it "should display an X in the bottom left row" do
+            @new_board.place_X(8)
+            output = @new_board.generate_board()
+            expect(output).to eq("   |   |   \n-----------\n   |   |   \n-----------\n   |   | X \n")
         end
     end
 
